466afe6831ca7283c0e51e84e1819befd832e11b
jpared3s <jparedes.wall@gmail.com>
2022-12-20 09:11:07 -0500

    <prep>

---
+++ tinkering.js
@@ -0,0 +1,14 @@
+function multiply(a, b) {
+  return a * b;
+}
+
+const result = multiply(5, 18);
+
+console.log(result);
+
+let names = "James";
+names[0];
+console.log(names);
+
+console.log("Hello" % 10);
+
