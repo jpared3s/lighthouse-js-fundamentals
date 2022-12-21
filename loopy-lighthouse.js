2b87c373aac1bd8c6c52950f4ddd2afd84831349
jpared3s <jparedes.wall@gmail.com>
2022-12-21 09:50:13 -0500

    prep module

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,3 +1,11 @@
-for (let x = 100; x <=200; x++) {
-  console.log(x);
+for(let x = 100; x <= 200; x++) {
+  if(x % 3 === 0 && x % 4 === 0) {
+    console.log("LoopyLighthouse");
+  }else if (x % 4 === 0) {
+    console.log("Lighthouse")
+  }else if (x % 3 === 0) {
+    console.log("Loopy");
+  }else {
+    console.log(x);
+  }
 }
