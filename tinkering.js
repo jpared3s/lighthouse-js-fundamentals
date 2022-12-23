5cc4418130f7b5c7b93e2bff1f2c93f076fc8eb1
jpared3s <jparedes.wall@gmail.com>
2022-12-22 20:12:42 -0500

    prep module

--- tinkering.js
+++ tinkering.js
@@ -1,14 +1,20 @@
-function multiply(a, b) {
-  return a * b;
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// const result = multiply(5, 18);
+
+// console.log(result);
+
+// let names = "James";
+// names[0];
+// console.log(names);
+
+// console.log("Hello" % 10);
+
+function isThisWorking(input) {
+  console.log("Printing: isThisWorking was called and " + input + " was passed in as an argument.");
+  return "Returning: I am returning this string!";
 }
 
-const result = multiply(5, 18);
-
-console.log(result);
-
-let names = "James";
-names[0];
-console.log(names);
-
-console.log("Hello" % 10);
-
+isThisWorking(3);
