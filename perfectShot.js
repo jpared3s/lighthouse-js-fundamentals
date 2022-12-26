dc8dbd07e015361220ece4da35e1ebdca6e42ad3
jpared3s <jparedes.wall@gmail.com>
2022-12-26 13:24:03 -0500

    perfectshot

---
+++ perfectShot.js
@@ -0,0 +1,20 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']  
+
+finalPosition = function (moves) {
+  let x = 0;
+  let y = 0;
+  for (const move of moves) {
+    if (move === 'north'){
+      y += 1;
+    } else if (move === 'south') {
+      y -= 1;
+    } else if (move === 'west') {
+      x -= 1;
+    } else if (move === 'east') {
+      x += 1;
+    }
+  }
+  return [x, y];
+}
+
+console.log(finalPosition)
